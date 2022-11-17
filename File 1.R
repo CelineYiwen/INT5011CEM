bbe7a279702a65ea2766ac7d9d002c45171c3e4c
Celine KY <P19011352@student.newinti.edu.my>
2022-11-17 19:38:23 +0800

    Initial Commit

--- File 1.R
+++ File 1.R
@@ -1,3 +1,105 @@
-# This is a test script.
 
-print("Hello World!")
+# ______________________________ Packages and Libraries ______________________________
+
+
+install.packages("data.table")
+install.packages("tidyverse")
+install.packages('microbenchmark')
+install.packages("parallel")
+
+install.packages("ggplot2")
+
+
+
+library(data.table)
+library(tidyverse)
+library(microbenchmark)
+library(parallel)
+
+library(ggplot2)
+
+
+
+
+
+# ______________________________ CHAPTER 2 ______________________________
+
+
+# Load Data And Read Multiple CSV File
+df <- function(i){
+  list.files(path = "C:/Users/valor/Downloads/Tesco Grocery 1.0 Dataset/Area-Level Grocery Purchases/Jan_borough_grocery/", pattern = "*.csv") %>% map_df(~fread(.))
+}
+df
+
+
+# Sequential Processing
+
+# Microbenchmark
+mbm <- microbenchmark(lapply(1:100, df))
+mbm
+autoplot(mbm)
+
+# System Time
+seq <- system.time(lapply(1:100, df))
+
+
+# Parallel Processing
+numCores <- detectCores()
+numCores
+
+cl <- makeCluster(numCores)
+
+clusterEvalQ(cl, {
+  library(tidyverse)
+  library(parallel)
+  library(data.table)
+})
+
+system.time({
+  parLapply(cl, 1:100, df)
+  stopCluster(cl)
+})
+
+
+
+
+
+
+# ______________________________ CHAPTER 3 ______________________________
+
+
+
+
+
+
+
+
+
+
+
+
+# ______________________________        ______________________________
+
+
+
+
+
+
+
+
+
+
+
+# ______________________________        ______________________________
+
+
+
+
+
+
+
+
+
+
+
+
