5bb8510ffc91d28bfe7ce8cf0c91f3d58779af75
Celine KY <P19011352@student.newinti.edu.my>
2022-11-17 22:15:08 +0800

    Second Commit

--- File 1.R
+++ File 1.R
@@ -1,39 +1,38 @@
 
 # ______________________________ Packages and Libraries ______________________________
 
-
 install.packages("data.table")
 install.packages("tidyverse")
 install.packages('microbenchmark')
 install.packages("parallel")
+install.packages("lme4")
+
 
 install.packages("ggplot2")
-
 
 
 library(data.table)
 library(tidyverse)
 library(microbenchmark)
 library(parallel)
+library(lme4)
 
 library(ggplot2)
 
 
 
 
-
 # ______________________________ CHAPTER 2 ______________________________
 
+# Read Multiple CSV File
 
-# Load Data And Read Multiple CSV File
 df <- function(i){
-  list.files(path = "C:/Users/valor/Downloads/Tesco Grocery 1.0 Dataset/Area-Level Grocery Purchases/Jan_borough_grocery/", pattern = "*.csv") %>% map_df(~fread(.))
+  list.files(path = "C:/Users/valor/Downloads/Tesco Grocery 1.0 Dataset/Area-Level Grocery Purchases/", pattern = "*.csv") %>% map_df(~fread(.))
 }
 df
 
 
 # Sequential Processing
-
 # Microbenchmark
 mbm <- microbenchmark(lapply(1:100, df))
 mbm
@@ -63,11 +62,18 @@ system.time({
 
 
 
-
-
 # ______________________________ CHAPTER 3 ______________________________
 
 
+# Load And Read Grocery Dataset
+groceryData_Borough <- read.csv("C:/Users/valor/Downloads/Tesco Grocery 1.0 Dataset/Obesity_Borough/")
+
+# Launch Dataset In RStudio Viewer
+View(groceryData_Borough)
+
+# Display the first and last 5 rows of data
+head(groceryData_Borough, 5)
+tail(groceryData_Borough, 5)
 
 
 
@@ -83,7 +89,7 @@ system.time({
 
 
 
-
+Obesity
 
 
 
